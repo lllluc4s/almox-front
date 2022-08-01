7bfcd39997db9cc795a6bcb33f4b3f2d8bb4a271
Lucas <lucasr4394@gmail.com>
2022-08-01 13:52:18 -0300

    alteração na rota de criação de usuário

--- pages/createUser.vue
+++ pages/createUser.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="w-1/2 mx-auto bg-white p-5 rounded-lg">
-    <!-- <Errors class="mb-5" :errors="errors"></Errors> -->
-
     <form method="post" autoComplete="off" @submit.prevent="submit">
       <div class="mt-4">
         <Label html-for="name">Nome</Label>
@@ -9,8 +7,6 @@
         <Input
           id="name"
           v-model="name"
-          :counter="100"
-          :rules="nameRules"
           type="string"
           class="block mt-1 w-full"
           required
@@ -19,21 +15,12 @@
         />
       </div>
 
-			<!-- <v-text-field
-        v-model="name"
-        :counter="100"
-        :rules="nameRules"
-        label="Nome"
-        required
-      ></v-text-field> -->
-
       <div class="mt-4">
         <Label html-for="email">Email</Label>
 
         <Input
           id="email"
           v-model="email"
-          :rules="emailRules"
           type="email"
           class="block mt-1 w-full"
           required
@@ -67,14 +54,7 @@ export default {
   data: () => ({
     valid: true,
     name: '',
-    nameRules: [(v) => !!v || 'Preencha o campo Nome'],
-
     email: '',
-    emailRules: [
-      (v) => !!v || 'Preencha o campo Email',
-      (v) => /.+@.+\..+/.test(v) || 'Formato de email inserido não é válido!',
-    ],
-
     password: '',
   }),
 
@@ -88,24 +68,26 @@ export default {
         password: this.password,
       }
 
-      if (this.$refs.form.valid()) {
+      if (this.valid) {
         axios
           .post('http://127.0.0.1:8000/api/users', data)
           .then(() => {
             alert('Opa, deu tudo certo! Conta criada com sucesso!')
           })
           .catch(() => {
-            alert('Ops, algo deu errado! Preencha o formulário corretamente.')
+            alert('Ops, algo deu errado! Tente novamente.')
           })
 
-        this.$refs.form.reset()
+        this.resetForm()
       } else {
         alert('Preencha os campos necessários!')
       }
     },
 
-    reset() {
-      this.$refs.form.reset()
+    resetForm() {
+      this.name = ''
+      this.email = ''
+      this.password = ''
     },
   },
 }
