db663cd1357a7b41d6c281c8d3c9b3795d052eec
Lucas <lucasr4394@gmail.com>
2022-08-02 17:55:01 -0300

    alteração nas visualizações

--- pages/equipments/_equipment.vue
+++ pages/equipments/_equipment.vue
@@ -1,76 +1,119 @@
 <template>
-	<div class="mt-20 flex">
-		<div class="w-1/2 h-80 relative overflow-hidden rounded-lg">
-			<img
-				class="absolute inset-0 w-full h-full object-cover"
-				:src="equipment.image"
-				alt="Imagem do equipamento"
-			/>
-		</div>
+  <div class="mt-20 flex">
+    <div class="w-1/2 h-80 relative overflow-hidden rounded-lg">
+      <img
+        class="absolute inset-0 w-full h-full object-cover"
+        :src="equipment.image"
+        alt="Imagem do equipamento"
+      />
+    </div>
 
-		<div class="w-full pl-14">
-			<div class="flex justify-between items-center mb-6">
-				<h1 class="text-2xl font-bold">{{ equipment.type }}</h1>
-				<span class="block font-semibold">{{ equipment.status }}</span>
-			</div>
-			<p class="leading-loose mb-5">
-				{{ equipment.patrimony }}
-			</p>
-			<button
-				class="rounded inline-flex items-center px-4 py-2 bg-purple-700 border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-800 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:ring ring-purple-300 disabled:opacity-25 transition ease-in-out duration-150"
-				@click="reservar"
-			>
-				Reservar
-			</button>
-			<button
-				class="rounded inline-flex items-center px-4 py-2 bg-purple-700 border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-800 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:ring ring-purple-300 disabled:opacity-25 transition ease-in-out duration-150"
-				@click="deletar"
-			>
-				Deletar
-			</button>
-		</div>
-	</div>
+    <div class="w-full pl-14">
+      <div class="flex justify-between items-center mb-6">
+        <h1 class="text-2xl font-bold">{{ equipment.type }}</h1>
+        <span class="block font-semibold">{{ equipment.status }}</span>
+      </div>
+      <p class="leading-loose mb-5">
+        {{ equipment.patrimony }}
+      </p>
+      <button
+        class="
+          rounded
+          inline-flex
+          items-center
+          px-4
+          py-2
+          bg-purple-700
+          border border-transparent
+          font-semibold
+          text-xs text-white
+          uppercase
+          tracking-widest
+          hover:bg-purple-800
+          active:bg-purple-900
+          focus:outline-none focus:border-purple-900 focus:ring
+          ring-purple-300
+          disabled:opacity-25
+          transition
+          ease-in-out
+          duration-150
+        "
+        @click="reservar"
+      >
+        Reservar
+      </button>
+      <button
+        class="
+          rounded
+          inline-flex
+          items-center
+          px-4
+          py-2
+          bg-purple-700
+          border border-transparent
+          font-semibold
+          text-xs text-white
+          uppercase
+          tracking-widest
+          hover:bg-purple-800
+          active:bg-purple-900
+          focus:outline-none focus:border-purple-900 focus:ring
+          ring-purple-300
+          disabled:opacity-25
+          transition
+          ease-in-out
+          duration-150
+        "
+        @click="deletar"
+      >
+        Deletar
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		equipment: {},
-	}),
+  data: () => ({
+    equipment: {},
+  }),
 
-	mounted() {
-		this.fetch()
+  mounted() {
+    this.fetch()
 
-		// this.head()
-	},
+    // this.head()
+  },
 
-	methods: {
-		async fetch() {
-			const response = await this.$axios.$get(
-				'equipments/' + this.$route.params.equipment
-			)
+  methods: {
+    async fetch() {
+      const response = await this.$axios.$get(
+        'equipments/' + this.$route.params.equipment
+      )
 
-			this.equipment = response
-		},
+      this.equipment = response
+    },
 
-		// head() {
-		//   return {
-		//     title: this.equipment.type + ' - Almoxarifado',
-		//   }
-		// },
+    // head() {
+    //   return {
+    //     title: this.equipment.type + ' - Almoxarifado',
+    //   }
+    // },
 
-		reservar() {
-			this.$axios.$post('bookings', {
-				equipment_id: this.equipment.id,
-			})
+    reservar() {
+      this.$axios.$post('bookings', {
+        equipment_id: this.equipment.id,
+      })
 
-			this.$router.push('/bookings')
-		},
+      this.$router.push('/')
+    },
 
-		deletar() {
-			console.log('deletar')
-			// this.$axios.$delete('equipments/' + this.equipment.id)
-		},
-	},
+    deletar() {
+      return confirm('Tem certeza que deseja deletar este equipamento?')
+        ? this.$axios
+            .$delete('equipments/' + this.equipment.id)
+            .then(() => this.$router.push('/'))
+        : null
+    },
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,28 @@
 <template>
-  <div>
+  <div class="grid grid-cols-3 gap-3">
     <div v-for="item in equipments" :key="item.id" class="m-20">
-      <div class="w-1/3 h-44 relative overflow-hidden rounded-lg">
+      <div>
         <img
-          class="absolute inset-0 w-full h-full object-cover"
+          class="w-full h-full"
           :src="item.image"
           alt="Imagem do equipamento"
         />
       </div>
 
-      <div class="w-full">
-        <div class="flex justify-between items-center mb-1">
-          <h1 class="text-2xl font-bold">{{ item.type }}</h1>
-          <span class="block font-semibold">{{ item.status }}</span>
-        </div>
-        <p class="leading-loose mb-4">
-          {{ item.patrimony }}
-        </p>
-        <NuxtLink
-          class="text-purple-600 font-bold"
-          :to="'/equipments/' + item.id"
-        >
-          Mais detalhes...
-        </NuxtLink>
+      <div>
+        <h1 class="text-2xl font-bold">{{ item.type }}</h1>
+        <span class="block font-semibold">{{ item.status }}</span>
       </div>
+      <p class="leading-loose mb-4">
+        Eiqueta de patrimônio:
+        {{ item.patrimony }}
+      </p>
+      <NuxtLink
+        class="text-purple-600 font-bold"
+        :to="'/equipments/' + item.id"
+      >
+        Opções
+      </NuxtLink>
     </div>
   </div>
 </template>
